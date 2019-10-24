df6bf9e6b99dd216763d9b4cf8453639f8a6ed8d
kalebch2 <55675424+kalebch2@users.noreply.github.com>
2019-10-24 11:06:53 -0500

    Lab 4 Complete

--- scripts/lab4_func.py
+++ scripts/lab4_func.py
@@ -8,6 +8,43 @@ Use 'expm' for matrix exponential.
 Angles are in radian, distance are in meters.
 """
 
+# For (1): Theta = -45 -45 90 -45 -90 90
+#	x = .3575
+#	y = .1245
+#	z = .2515
+
+# For (2): Theta = 0 -90 90 0 -90 90
+#	x = .4141
+#	y = -.2465
+#	z = .02855
+
+def skewS(S):
+	w = S[0:3]
+	v = S[3:6]
+	
+	SS = np.zeros((4,4))
+
+	SS[0,0] = 0
+	SS[0,1] = -w[2]
+	SS[0,2] = w[1]
+	SS[0,3] = v[0]
+	SS[1,0] = w[2]
+	SS[1,1] = 0
+	SS[1,2] = -w[0]
+	SS[1,3] = v[1]
+	SS[2,0] = -w[1]
+	SS[2,1] = w[0]
+	SS[2,2] = 0
+	SS[2,3] = v[2]
+	SS[3,0] = 0
+	SS[3,1] = 0
+	SS[3,2] = 0
+	SS[3,3] = 0
+
+	SS = np.asarray(SS, dtype=np.float64)
+
+	return SS
+
 def Get_MS():
 	# =================== Your code starts here ====================#
 	# Fill in the correct values for a1~6 and q1~6, as well as the M matrix
@@ -42,9 +79,17 @@ def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
 	# =================== Your code starts here ====================#
 	theta = np.array([theta1,theta2,theta3,theta4,theta5,theta6])
 	T = np.eye(4)
+	T = np.asarray(T, dtype=np.float64)
 
 	M, S = Get_MS()
 
+	T = expm(skewS(S[:,0])*theta1)
+	T = np.matmul(T, expm(skewS(S[:,1])*theta2))
+	T = np.matmul(T, expm(skewS(S[:,2])*theta3))
+	T = np.matmul(T, expm(skewS(S[:,3])*theta4))
+	T = np.matmul(T, expm(skewS(S[:,4])*theta5))
+	T = np.matmul(T, expm(skewS(S[:,5])*theta6))
+	T = np.matmul(T,M)
 
 	# ==============================================================#
 	
@@ -58,6 +103,7 @@ def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
 	return_value[5] = theta6
 
 	return return_value
+
 
 
 
